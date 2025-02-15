4292162e2bf03c8ee84bb3b27e014d30049e5e9b
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-02-15 11:53:22 +0800

    re added start

--- honoka_bot.py
+++ honoka_bot.py
@@ -11,20 +11,20 @@ TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Start the Minecraft server
-# @bot.command(name="start")
-# async def start_server(ctx):
-#     await ctx.send("Starting the Minecraft server... Wait, okay?\n https://tenor.com/view/love-live-school-idol-project-honoka-kousaka-trumpet-honk-gif-19422260")
+#Start the Minecraft server
+@bot.command(name="start")
+async def start_server(ctx):
+    await ctx.send("Starting the Minecraft server... Wait, okay?\n https://tenor.com/view/love-live-school-idol-project-honoka-kousaka-trumpet-honk-gif-19422260")
 
-#     # Check if the server is already running
-#     result = subprocess.run(["pgrep", "-f", "server.jar"], capture_output=True, text=True)
-#     if result.stdout.strip():
-#         await ctx.send("Minecraft server is already running desu yo!! https://tenor.com/view/umi-honoka-gif-25604008")
-#         return
+    # Check if the server is already running
+    result = subprocess.run(["pgrep", "-f", "server.jar"], capture_output=True, text=True)
+    if result.stdout.strip():
+        await ctx.send("Minecraft server is already running desu yo!! https://tenor.com/view/umi-honoka-gif-25604008")
+        return
 
-#     # Start server using tmux
-#     subprocess.run(["tmux", "new-session", "-d", "-s", "minecraft", "java -Xms2G -Xmx4G -jar server.jar nogui"])
-#     await ctx.send("Minecraft server started!! Let's play!! https://tenor.com/view/lets-goo-honoka-kousaka-gif-23293886")
+    # Start server using tmux
+    subprocess.run(["tmux", "new-session", "-d", "-s", "minecraft", "java -Xms2G -Xmx4G -jar server.jar nogui"])
+    await ctx.send("Minecraft server started!! Let's play!! https://tenor.com/view/lets-goo-honoka-kousaka-gif-23293886")
 
 @bot.command(name="fd")
 async def faitodayo(ctx):
